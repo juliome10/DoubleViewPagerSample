59288cbf29cf656f88b8e4ddcb18f5c784fc86bb
Julio Gomez <juliome10@msn.com>
2015-05-22 13:47:32 +0200

    Initial commit

---
+++ DoubleViewPagerSample/src/com/emoiluj/doubleviewpagersample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.emoiluj.doubleviewpagersample;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+
+import com.emoiluj.doubleviewpager.DoubleViewPager;
+import com.emoiluj.doubleviewpager.DoubleViewPagerAdapter;
+
+
+public class MainActivity extends Activity{
+
+	private DoubleViewPager viewpager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		loadUI();
+	}
+
+	private void loadUI() {
+
+		ArrayList<PagerAdapter> verticalAdapters = new ArrayList<PagerAdapter>();
+		generateVerticalAdapters(verticalAdapters);
+
+		viewpager = (DoubleViewPager) findViewById(R.id.pager);
+		viewpager.setAdapter(new DoubleViewPagerAdapter(getApplicationContext(), verticalAdapters));
+	}
+
+	private void generateVerticalAdapters(ArrayList<PagerAdapter> verticalAdapters) {
+		//5 in horizontal
+		for (int i=0; i<5; i++){
+			verticalAdapters.add(new VerticalPagerAdapter(this, i));
+		}
+	}
+
+}
---
+++ DoubleViewPagerSample/src/com/emoiluj/doubleviewpagersample/VerticalPagerAdapter.java
@@ -0,0 +1,75 @@
+package com.emoiluj.doubleviewpagersample;
+
+import java.util.Random;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.RelativeLayout.LayoutParams;
+
+import com.emoiluj.doubleviewpager.VerticalViewPager;
+
+public class VerticalPagerAdapter extends PagerAdapter{
+
+	private Context mContext;
+	private int mParent;
+
+	public VerticalPagerAdapter(Context c, int parent){
+		mContext = c;
+		mParent = parent;
+	}
+
+	@Override
+	public int getCount() {
+		return 5;
+	}
+
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return view == ((View) object);
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+
+		RelativeLayout relative = new RelativeLayout(mContext);
+		LayoutParams relativelp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		relative.setLayoutParams(relativelp);
+		relative.setGravity(Gravity.CENTER);
+
+		TextView tv = new TextView(mContext);
+		LayoutParams tvlp=new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		tvlp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+		tv.setLayoutParams(tvlp);
+		tv.setText("P:"+mParent + " H:"+position);
+		tv.setTextSize(70);
+		setColors(relative, tv);
+		relative.addView(tv);
+
+		container.addView(relative);
+		return relative;
+	};
+
+	public void destroyItem(View container, int position, Object view) {
+		((VerticalViewPager) container).removeView((View) view);
+	};
+
+	public void setColors(RelativeLayout layout, TextView text){
+		Random rand = new Random();
+		int r = rand.nextInt(255);
+		int g = rand.nextInt(255);
+		int b = rand.nextInt(255);
+		
+		layout.setBackgroundColor(Color.rgb(r,g,b));
+		if(r<30 || g<30 || b<30){
+			text.setTextColor(Color.WHITE);
+		}else{
+			text.setTextColor(Color.BLACK);
+		}
+	}
+}
